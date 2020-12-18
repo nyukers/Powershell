bd4b8674ba274426a3057ccba28a553e087e92bf
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-12-18 13:53:27 +0200

    Add files via upload

---
+++ Workflows/WorkFlow01.ps1
@@ -0,0 +1,61 @@
+﻿Workflow Refresh-MyApp
+
+{
+
+  sequence {
+
+    Invoke-Command -ComputerName WEB1 {Stop-Process -Name MyApp -Force}
+    Invoke-Command -ComputerName WEB1 {Stop-Process -Name MyAppQueue -Force}
+
+    parallel {
+
+      Invoke-Command -ComputerName FS1 {Remove-Item -Path «D:\MyApp\Files\MyAppQueue.xml» –Force}
+      Invoke-Command -ComputerName FS2 {Remove-Item –Path «D:\MyApp\Files\MyAppQueue.xml» –Force}
+
+      sequence {
+
+        parallel {
+
+          Invoke-Command -ComputerName FS1 {Stop-Process -Name MyAppFSQueue}
+          Invoke-Command -ComputerName FS2 {Stop-Process -Name MyAppFSQueue}
+
+        }
+
+        Invoke-Command -ComputerName FS1 {Remove-Item –Path «D:\MyApp\Log\Queue.log» –Force}
+        Invoke-Command -ComputerName FS2 {Remove-Item –Path «D:\MyApp\Log\Queue.log» –Force}
+
+        parallel {
+
+          Invoke-Command -ComputerName SQL1 {Stop-Process -Name mssqlserver -Force}
+          Invoke-Command -ComputerName SQL2 {Stop-Process -Name mssqlserver -Force}
+          Invoke-Command -ComputerName SQL3 {Stop-Process -Name mssqlserver -Force}
+
+        }
+
+        parallel {
+
+          Invoke-Command -ComputerName SQL1 {Start-Process -Name mssqlserver -Force}
+          Invoke-Command -ComputerName SQL2 {Start-Process -Name mssqlserver -Force}
+          Invoke-Command -ComputerName SQL3 {Start-Process -Name mssqlserver -Force}
+
+        }
+
+        sequence {
+
+          Invoke-Command -ComputerName FS1 {Start-Process -Name MyAppFSQueue}
+          Invoke-Command -ComputerName FS2 {Start-Process -Name MyAppFSQueue}
+
+        }
+
+      }
+
+    }
+
+    Invoke-Command -ComputerName WEB1 {Start-Process -Name MyAppQueue}
+    Invoke-Command -ComputerName WEB1 {Start-Process -Name MyApp}
+
+  }
+
+  Send-MailMessage -From «MyApp@domain.com» -To «servicedesk@domain.com» -Subject «MyApp maintenance completed» -Body «The MyApp maintenance script has run.»
+
+}
---
+++ Workflows/WorkFlow02.ps1
@@ -0,0 +1,31 @@
+﻿workflow New-VirtualMachines
+
+{
+
+  [CmdletBinding()]
+
+  Param (
+
+    [Parameter(Mandatory=$true,Position=0)]$VMName,
+
+    [Parameter(Mandatory=$true,Position=1)]$VMCount,
+
+    [Parameter(Mandatory=$true,Position=2)]$VHDSize
+
+  )
+
+  $VMs = 1..$VMCount
+
+  foreach -parallel ($VM in $VMs)
+
+  {
+
+    New-VM -Name $VMName$VM -MemoryStartupBytes 512MB -NewVHDPath «D:\Hyper-V\Virtual Hard Disks\$VMName$VM.vhdx» -NewVHDSizeBytes $VHDSize
+
+  }
+
+}
+
+# Вы можете вызвать этот рабочий процесс так же, как и функцию:
+
+New-VirtualMachines -VMName VM -VMCount 5 -VHDSize 15GB
