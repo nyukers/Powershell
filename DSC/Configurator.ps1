6febc7cb324e43cce7aadbf72df1315e64e7d096
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-12-18 13:52:39 +0200

    Add files via upload

---
+++ DSC/Configurator.ps1
@@ -0,0 +1,36 @@
+﻿Start-DscConfiguration
+Start-DscConfiguration -Path 'D:\Reports' -Wait -Verbose
+
+Get-DscConfiguration
+
+Get-DscResource
+
+###
+Get-DscLocalConfigurationManager
+
+Get-WmiObject -Namespace "Root\Microsoft\Windows\DesiredStateConfiguration" -List
+
+###
+# Import-DSCResource –ModuleName PSDesiredStateConfiguration 
+# Import-DscResource -ModuleName PSDscResources
+
+Configuration LocalConfigurationManager
+{
+    Node "localhost"
+    {
+        LocalConfigurationManager
+        {
+	        ConfigurationMode = "ApplyOnly"
+            ConfigurationModeFrequencyMins = 45
+            DownloadManagerName = "WebDownloadManager"
+            RefreshMode = "Pull"
+            RefreshFrequencyMins = 90
+            RebootNodeIfNeeded = $false
+            AllowModuleOverwrite = $false
+        }
+    }
+}
+
+LocalConfigurationManager -OutputPath "D:\Reports"
+
+Set-DscLocalConfigurationManager -Path "D:\Reports"
