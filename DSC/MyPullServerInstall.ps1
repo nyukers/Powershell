1876bb318ee51feb8998d02a5db5ca5d35d58a8f
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-12-19 20:52:50 +0200

    Add files via upload

--- DSC/MyPullServerInstall.ps1
+++ DSC/MyPullServerInstall.ps1
@@ -28,7 +28,22 @@ $psclientid = Get-DscLocalConfigurationManager -CimSession $pscs | select -Expan
 Get-ChildItem -Path C:\scripts\MOF\GUEST01.mof | Rename-Item -NewName "C:\scripts\MOF\$psclientid.mof"
 New-DscChecksum -Path "C:\scripts\MOF\$psclientid.mof" -Force
 
-#The MOF and checksum files
+# The MOF and checksum files
 #5827c542-20bb-487c-89cb-484cbe5f0b1f.mof
 #5827c542-20bb-487c-89cb-484cbe5f0b1f.mof.checksum
+
+# Copy guest's MOF-files (zip) to Pull server
+$s = New-PSSession -ComputerName GOVERLA
+Get-ChildItem -Path .\MOF\ -Filter "$psclientid.*" | Copy-Item -Destination "C:\program Files\WindowsPowerShell\DscService\Configuration" `
+-ToSession $s -Force
+
+$module = Get-Module -ListAvailable xSmbShare
+$modulepath = "$(Split-Path -Path $module.Path)\*"
+$moduleversion = $module.Version.ToString()
+Compress-Archive -Path $modulepath -DestinationPath "C:\scripts\ModuleZips\xSMBShare_$moduleversion.zip" -Force
+
+New-DscChecksum -Path "C:\scripts\ModuleZips\xSMBShare_$moduleversion.zip" -Force
+
+Get-ChildItem -Path "C:\scripts\ModuleZips\xSMBShare_$moduleversion.*" | Copy-Item -Destination "C:\Program Files\WindowsPowerShell\DscService\Modules\" `
+-ToSession $s -Force
 
