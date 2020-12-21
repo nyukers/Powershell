c428141ddeacc4fac1ef43adf496f22bc7a02349
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-12-21 08:38:05 +0200

    Add files via upload

--- DSC/HomeTest18.2.ps1
+++ DSC/HomeTest18.2.ps1
@@ -1,11 +1,12 @@
-﻿Configuration AddFile {
+﻿# My configuration AddFile
+Configuration AddFile {
 
     param (
     [Parameter(Mandatory=$true)]
     [string[]]$ComputerName 
     )
 
-Import-DscResource –ModuleName "PSDesiredStateConfiguration"
+Import-DscResource –ModuleName PSDesiredStateConfiguration
 
     Node $ComputerName { 
         File TestFile { 
@@ -17,29 +18,33 @@ Import-DscResource –ModuleName "PSDesiredStateConfiguration"
                       }
                   }
 }
+
+# Generate MOF-file
 AddFile -OutputPath c:\VM\shared\PS\DSC -ComputerName 'GOVERLA01','GOVERLA'
 
+# Check MOF-file
 Test-DscConfiguration -ComputerName GOVERLA -verbose `
 -ReferenceConfiguration c:\VM\shared\PS\DSC\MOF\GOVERLA.mof |
 Format-List
 
+# Apply MOF-file to host GOVERLA
 Start-DscConfiguration -ComputerName GOVERLA -Path c:\VM\shared\PS\DSC\MOF -Wait -Verbose
 
 
 Get-Job -IncludeChildJob
 
+# Check applied configuration on host GOVERLA 
 Get-DscConfiguration -CimSession GOVERLA | 
 Format-Table PSComputerName, ConfigurationName, Ensure, Type -AutoSize
 
+# Check current configuration host GOVERLA
+$cs = New-CimSession -ComputerName GOVERLA
+Get-DscConfiguration -CimSession $cs
+Remove-CimSession -CimSession $cs
+
+# Check result
 Invoke-Command -ComputerName GOVERLA -ScriptBlock {
 Get-Content -Path c:\testfolder\testfile1.txt
 }
-
-#check current configuration
-$cs = New-CimSession -ComputerName GOVERLA
-Get-DscConfiguration -CimSession $cs
-
-Remove-CimSession -CimSession $cs
-
 
 Test-Path -Path \\GOVERLA\C$\TestFolder\TestFile1.txt
