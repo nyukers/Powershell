7ab13ca019f427f59727272a88b8e1cead41e361
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-12-21 09:58:32 +0200

    Add files via upload

---
+++ DSC/HomeTest18.9.ps1
@@ -0,0 +1,26 @@
+﻿$cs = New-CimSession -ComputerName GOVERLA
+Get-DscLocalConfigurationManager -CimSession $cs
+
+Remove-CimSession -CimSession $cs
+
+[DSCLocalConfigurationManager()]
+    Configuration LCM {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string[]]$ComputerName
+    )
+    Node $Computername
+    {
+    Settings
+    {
+        ConfigurationMode = 'ApplyAndAutoCorrect'
+        RebootNodeIfNeeded = $false
+    }
+    }
+}
+
+LCM -computername GOVERLA -OutputPath .\MOF
+
+Set-DscLocalConfigurationManager -CimSession $cs -Path .\MOF\ -Verbose
+
+Get-DscLocalConfigurationManager -CimSession $cs | select ConfigurationMode, RefreshMode, RefreshFrequencyMins, RebootNodeIfNeeded | fl
