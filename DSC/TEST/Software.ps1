3dd5fa3e70adeb4d366d45e9ada864ecff71d305
Nyukers <nyukers@gmail.com>
2020-12-28 16:25:36 +0200

    Add files via upload

---
+++ DSC/TEST/Software.ps1
@@ -0,0 +1,22 @@
+﻿Import-DSCResource –ModuleName PSDesiredStateConfiguration 
+
+configuration Software {
+
+    param (
+        [String[]]$Server=$env:computerName
+    )
+ 
+    Node $Server {
+ 
+        File SetupFile {
+ 
+            Ensure          = "present"
+            SourcePath      = "D:\test.cmd"
+            DestinationPath = "D:\Reports"
+            Type            = "File"
+ 
+        }
+    }
+}
+ 
+Software -Server ("srv01","srv02")
