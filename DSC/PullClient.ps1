5270f454d9a4faf31f99e70f1161c5a00cf63929
Nyukers <nyukers@gmail.com>
2020-12-28 15:46:42 +0200

    Add files via upload

---
+++ DSC/PullClient.ps1
@@ -0,0 +1,24 @@
+﻿Configuration LCMPullConfig 
+{ 
+    LocalConfigurationManager 
+    { 
+        ConfigurationID = "VSUDO550"; 
+        RefreshMode = "PULL";
+        DownloadManagerName = "WebDownloadManager";
+        RebootNodeIfNeeded = $false;
+        RefreshFrequencyMins = 60;
+        ConfigurationModeFrequencyMins = 30; 
+        ConfigurationMode = "ApplyAndAutoCorrect";
+        DownloadManagerCustomData = @{
+        ServerUrl = "http://puller.forza.com:8080/PSDSCPullServer/PSDSCPullServer.svc"; 
+        AllowUnsecureConnection = "TRUE"}
+    } 
+} 
+
+# Create the .mof meta file for the target node
+LCMPullConfig -OutputPath "D:\Reports"
+
+# We're essentially turning on Pull Mode on the Target
+Set-DSCLocalConfigurationManager -Computer localhost -Path "D:\Reports" -Verbose
+
+Get-DscLocalConfigurationManager
