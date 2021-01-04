28ab5911f9093ca5f52372095f5f5c9b3fb78a9d
Nyukers <nyukers@gmail.com>
2021-01-04 10:46:05 +0200

    Add files via upload

---
+++ WinRM/PSWA-rules.ps1
@@ -0,0 +1,20 @@
+﻿Install-WindowsFeature -Name WindowsPowerShellWebAccess -IncludeManagementTools
+
+Install-PswaWebApplication -UseTestCertificate
+
+#Add-PswaAuthorizationRule –UserGroupName forza-log01\Administrators -ComputerName forza-log01 -ConfigurationName Microsoft.PowerShell
+
+Add-PswaAuthorizationRule –UserGroupName 'forza.com\DL-Srv-Local-Admins' -ComputerName forza-ep01 -ConfigurationName Microsoft.PowerShell
+Add-PswaAuthorizationRule –UserGroupName 'forza.com\DL-Srv-Local-Admins' -ComputerName forza-log01 -ConfigurationName Microsoft.PowerShell
+
+
+Add-PswaAuthorizationRule –UserGroupName 'forza.com\DL-Srv-Local-Admins' -ComputerGroupName 'forza.com\Infrastructure Servers' -ConfigurationName Microsoft.PowerShell
+
+Add-PswaAuthorizationRule -UserName forza.com\MikeCrouly.EA -ComputerName * -ConfigurationName Microsoft.PowerShell
+
+Test-PswaAuthorizationRule -UserName forza.com\MikeCrouly.EA-MSA -ComputerName forza-log01 -ConfigurationName Microsoft.PowerShell
+
+Get-PswaAuthorizationRule 
+
+Enable-PsRemoting
+
---
+++ WinRM/winrm.cmd
@@ -0,0 +1,3 @@
+:winrm delete winrm/config/Listener?Address=*+Transport=HTTPS
+
+winrm create winrm/config/Listener?Address=*+Transport=HTTPS
