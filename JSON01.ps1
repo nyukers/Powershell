db676693403ea6c45ef1f1f833e0fd67ee0c7535
Nyukers <nyukers@gmail.com>
2021-11-10 12:12:13 +0200

    Add files via upload

---
+++ JSON01.ps1
@@ -0,0 +1,76 @@
+﻿# 1
+$User = @"
+{
+   "firstName": "Иван",
+   "lastName": "Иванов",
+   "address": {
+       "streetAddress": "Московское ш., 101, кв.101",
+       "city": "Ленинград",
+       "postalCode": 101101
+   },
+   "phoneNumbers": [
+       "812 123-1234",
+       "916 123-4567"
+   ]
+}
+"@ | ConvertFrom-Json
+
+$User.firstName
+$User.phoneNumbers[0]
+
+# 2
+$json = @"
+{
+  "ServerName"        : "$env:ComputerName",
+  "UserName"          : "$env:UserName",
+  "ComputerInfo"      :
+  {
+    "Manufacturer": "$((Get-WmiObject Win32_ComputerSystem).Manufacturer)",
+    "Architecture": "$((Get-wmiObject Win32_OperatingSystem).OSArchitecture)",
+    "SerialNumber": "$((Get-wmiObject Win32_OperatingSystem).SerialNumber)"
+  },
+  "CollectionDate"    : "$(Get-Date)"
+ }
+"@
+
+$Info = ConvertFrom-Json -InputObject $json
+$Info.ServerName
+$Info.ComputerInfo.Manufacturer
+$Info.ComputerInfo.Architecture
+
+# 3
+$File = Get-ChildItem C:\Windows\System32\calc.exe
+$File.VersionInfo | ConvertTo-Json
+$File.VersionInfo | fl -Property *
+
+# 4
+$response = Invoke-WebRequest -Uri "https://fixmypc.ru/json.html"
+$response | Get-Member
+$response.Content
+$json = ConvertFrom-Json -InputObject $response.Content
+$json | Get-Member
+$json.employee
+
+# {"employee":{"name":"sonoo","salary":[56000, 30000],"married":true}}
+$json = @"
+{
+"employee":
+    {
+    "name": "sonar",
+    "salary":[
+              56000, 
+              30000
+             ],
+    "married":true
+    }
+}
+"@
+$Info = ConvertFrom-Json -InputObject $json
+$info.employee.name
+$info.employee.Salary[0]
+ConvertFrom-Json -InputObject $json | SELECT -ExpandProperty Employee
+
+ConvertTo-Json -InputObject (Get-Service)
+
+# 5
+(Get-Content -Path C:\vm\shared\ps\1233\1.json -Raw | ConvertFrom-Json).employee
---
+++ REST01.ps1
@@ -0,0 +1,129 @@
+﻿# GET
+ Invoke-RestMethod -Method GET -ContentType "application/json" -Uri "https://petstore.swagger.io/v2/pet/19"
+
+# POST
+$Body = @{
+    id = 19
+    category = @{
+        id = 45
+        name = "Whatever"
+    }
+    name = "Dawg"
+    photoUrls = @(
+        "string"
+    )
+    tags = @(
+        @{
+            id = 0
+            name = "string"
+        }
+    )
+    status = "available"
+}
+
+$JsonBody = $Body | ConvertTo-Json
+$Uri = "https://petstore.swagger.io/v2/pet"
+Invoke-RestMethod -ContentType "application/json" -Uri $Uri -Method POST -Body $JsonBody
+
+# DELETE
+Invoke-RestMethod -ContentType "application/json" -Uri "https://petstore.swagger.io/v2/pet/1" -Method DELETE
+
+# PUT
+$Body = [PSCustomObject]@{
+    id = 19
+    name = "Dawg is a cool boy"
+}
+
+$JsonBody = $Body | ConvertTo-Json
+$Uri = "https://petstore.swagger.io/v2/pet"
+Invoke-RestMethod -ContentType "application/json" -Uri $Uri -Method PUT -Body $JsonBody
+
+# Function GET
+Function Get-PetstorePet {
+    [cmdletbinding()]
+    param(
+        # Id of the pet
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [int]$Id
+    )
+    Begin{}
+    Process{
+        $RestMethodParams = @{
+            Uri = "https://petstore.swagger.io/v2/pet/$Id"
+            ContentType = "application/json"
+            Method = "GET"
+        }
+        Invoke-RestMethod @RestMethodParams
+    }
+    End{}
+}
+
+Get-PetstorePet -Id 19
+
+# Function POST
+Function Add-PetstorePet {
+    [cmdletbinding()]
+    param(
+        # Id of the pet
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [int]$Id,
+        # Name of the pet
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [string]$Name,        
+        # Status of the pet (available, sold etc)
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [string]$Status,        
+        # Id of the pet category
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [int]$CategoryId,        
+        # Name of the pet category
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [string]$CategoryName,        
+        # URLs to photos of the pet
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [string[]]$PhotoUrls,
+        # Tags of the pets as hashtable array: @{Id=1;Name="Dog"}
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Hashtable[]]$Tags
+    )
+    Begin{}
+    Process{
+        $Body = @{
+            id = $Id
+            category = @{
+                id = $CategoryId
+                name = $CategoryName
+            }
+            name = $Name
+            photoUrls = $PhotoUrls
+            tags = $Tags
+            status = $Status
+        }
+        $BodyJson = $Body | ConvertTo-Json
+        $RestMethodParams = @{
+            Uri = "https://petstore.swagger.io/v2/pet/"
+            ContentType = "application/json"
+            Method = "Post"
+            Body = $BodyJson
+        }
+        Invoke-RestMethod @RestMethodParams
+    }
+    End{}
+}
+
+$AddPetStorePetsParams = @{
+    Id = 44
+    Name = "Birdie"
+    Status = "available"
+    CategoryId = 50
+    CategoryName = "Hawks"
+    PhotoUrls = "https://images.contoso.com/hawk.jpg"
+    Tags = @(
+        @{
+            Id=10
+            Name="Not eagles"
+        }
+    )
+}
+
+Add-PetStorePet @AddPetStorePetsParams
