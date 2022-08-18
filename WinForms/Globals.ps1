37e39b33ca097ed5ccaca02b16221c24851e18b5
Nyukers Ltd <nyukers@gmail.com>
2022-08-18 12:04:34 +0300

    Add files via upload

---
+++ WinForms/Globals.ps1
@@ -0,0 +1,41 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+$hostpath  = "Hosts.csv"
+$logpath   = "CmRcViewerSessions.log"
+$logsystem = "C:\Windows\CCM\Logs\CmRcService.log"
+$cmpath    = "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\i386\CmRcViewer.exe"
+$cmpath2   = "C:\Program Files (x86)\Microsoft Endpoint Manager\AdminConsole\bin\i386\CmRcViewer.exe"
+$cmtracepath = "C:\Windows\CCM\CmTrace.exe"
+
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{
+<#
+	.SYNOPSIS
+		Get-ScriptDirectory returns the proper location of the script.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+	[OutputType([string])]
+	param ()
+	if ($null -ne $hostinvocation)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+#Sample variable that provides the location of the script
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+
+
